66079abb0336334009321a3ea135995a943ad726
Beniditto Eka Viyantyo <134614584+BenidittoV@users.noreply.github.com>
2025-09-24 21:11:24 +0700

    Update CompetitionSection.tsx

--- src/components/LandingPages/CompetitionSection.tsx
+++ src/components/LandingPages/CompetitionSection.tsx
@@ -29,8 +29,144 @@ const COMPETITIONS: CompetitionItem[] = [
     title: "Coswalk Competition",
     imageUrl:
       "https://drive.google.com/file/d/1ENLn7brOHSQvYTxrcawZmqfIYBHHGyzB/view",
+    href: "https://www.instagram.com/p/DOEJTfHkhX8/?img_index=1",
+    tag: "Kompetisi",
+    useIframe: true, 
+  },
+  {
+    title: "Lomba Menggambar Tingkat SD",
+    imageUrl:
+      "https://drive.google.com/file/d/1fJluMkAVhb06ZeQ_-L2dIFuPZtpySAgQ/view",
     href: "https://docs.google.com/forms/d/e/1FAIpQLSfT_hyucRsmfn8d5XcJbos2Qeb8S-OSGzv_3vJM1R_mnRmfqw/viewform",
     tag: "Kompetisi",
+    useIframe: true, 
+  },
+  {
+    title: "Talkshow: Racking Up Riches Via Freelancing In Video Game Development",
+    imageUrl:
+      "https://drive.google.com/file/d/19bja3UHmwUjGpc4LMDCHVdSkjqRjO9KD/view",
+    href: "https://www.instagram.com/p/DO-SsiVidS6/",
+    tag: "Seminar",
+    useIframe: true, 
+  },
+  {
+    title: "Pegafest Band Competition",
+    imageUrl:
+      "https://drive.google.com/file/d/1-iUh_khSKQYEqc0x_NAjhv4ZrTfBzsk_/view",
+    href: "https://www.instagram.com/p/DOSJJ9aj7D2/",
+    tag: "Kompetisi",
+    useIframe: true, 
+  },
+  {
+    title: "Lomba Foto & Video",
+    imageUrl:
+      "https://drive.google.com/file/d/1b_Bfx4CrLsaQltPwFYbGJiLuZQHIXvXd/view",
+    href: "https://www.instagram.com/p/DOvGtNfCaEU/?img_index=1",
+    tag: "Kompetisi",
+    useIframe: true, 
+  },
+  {
+    title: "Speed Drawing Contest",
+    imageUrl:
+      "https://drive.google.com/file/d/17SctUrmgyzxzClN3NLHUX6iaFzBnx_5z/view",
+    href: "https://docs.google.com/forms/d/e/1FAIpQLSenu6fS8h_e8kP-oKFcVAYehpATJk1J04GCd1o895XQdQLOFw/viewform",
+    tag: "Kompetisi",
+    useIframe: true, 
+  },
+  {
+    title: "Street Fighter Tournament",
+    imageUrl:
+      "https://drive.google.com/file/d/1UzHLoSJHLr1I4qHmayTXUKMmmNgnHvIv/view",
+    href: "https://www.instagram.com/p/DOVDBSKkg08/",
+    tag: "Kompetisi",
+    useIframe: true, 
+  },
+  {
+    title: "Kompetisi Robot Soccer",
+    imageUrl:
+      "https://drive.google.com/file/d/1bk56SLp0NagdusJJN4CkvWpEJEJTmymP/view",
+    href: "https://docs.google.com/forms/d/e/1FAIpQLScaRYl0345u8mHUyVkvrM-leZFxIu_nVOFme2AVgb32pvkO-A/viewform",
+    tag: "Kompetisi",
+    useIframe: true, 
+  },
+  {
+    title: "Seminar Komik Bareng Dosen ISI Yogyakarta",
+    imageUrl:
+      "https://drive.google.com/file/d/1z_XtXC4c6rdFhq6BTy61_sdi4M3Y4Lxb/view",
+    href: "https://docs.google.com/forms/d/e/1FAIpQLSdlJBi50JwcPswSh_L5nVqUjLyGXsYxyew9jNuTHl5a6QN63w/viewform",
+    tag: "Seminar",
+    useIframe: true, 
+  },
+  {
+    title: "Membuat Film Animasi Kelas Dunia Dengan Post Production",
+    imageUrl:
+      "https://drive.google.com/file/d/1ntDDPmKMmfVbns2mung32T-qHxc7IdlA/view",
+    href: "https://docs.google.com/forms/d/e/1FAIpQLSeNur2lEWCQ3uUdvYFMDU3ajCWSuQmcKBoAMgVUX3oIXksiew/viewform",
+    tag: "Seminar",
+    useIframe: true, 
+  },
+  {
+    title: "Hulon Yogyakomtek: Poteinsi IP(Intellectual Property) Karakter Komik",
+    imageUrl:
+      "https://drive.google.com/file/d/13PJrCHbCjLpSANmjVMo2tTdcGzecAU4F/view",
+    href: "https://bit.ly/seminarhuion",
+    tag: "Seminar",
+    useIframe: true, 
+  },
+  {
+    title: "Hulon Yogyakomtek: Dari Imaginasi ke Industri dengan Concept Art untuk Games/ VFX",
+    imageUrl:
+      "https://drive.google.com/file/d/1Mu6KSUKodvebNUGtRNySZabBK4MQlxAC/view",
+    href: "https://bit.ly/seminarhuion",
+    tag: "Seminar",
+    useIframe: true, 
+  },
+  {
+    title: "Seminar Komik: Jadi Komikus Itu Harus Serius",
+    imageUrl:
+      "https://drive.google.com/file/d/1A_mwzDLFMdorQCYlvGAI_DV0um7fyVT2/view",
+    href: "https://bit.ly/seminarhuion",
+    tag: "Seminar",
+    useIframe: true, 
+  },
+  {
+    title: "Game Changer",
+    imageUrl:
+      "https://drive.google.com/file/d/1ef1eQvGhiFigmreR8kzlPAB8NZJ-fY52/view",
+    href: "https://event.kimcommunication.com/eb14e19c",
+    tag: "Seminar",
+    useIframe: true, 
+  },
+  {
+    title: "Coswalk Competition",
+    imageUrl:
+      "https://drive.google.com/file/d/1Z7g0VR_WAEEAZVDeDNzwESQc0JQpeH7x/view",
+    href: "https://bit.ly/DaftarSeminarRobotik",
+    tag: "Seminar",
+    useIframe: true, 
+  },
+  {
+    title: "Showcase KPOP Dance",
+    imageUrl:
+      "https://drive.google.com/file/d/1Ki7akqAV2sfaB4pdHe-HAzqIw9JfkLSL/view",
+    href: "https://www.instagram.com/p/DOP41D8ETnm/?img_index=1",
+    tag: "Seminar",
+    useIframe: true, 
+  },
+  {
+    title: "Workshop Kurasi 1 On 1",
+    imageUrl:
+      "https://drive.google.com/file/d/1r00k1SLQcbV7aYY1I1lCwmk0ZnCyb-8n/view",
+    href: "https://docs.google.com/forms/d/e/1FAIpQLSeJdgNouVkpQAeImWTa2eHzu-DjSi8uyvq7CfxU9OJO_35JhA/viewform",
+    tag: "Seminar",
+    useIframe: true, 
+  },
+  {
+    title: "Smart Tech, Smarter Future. Artifical Intelligence Now!",
+    imageUrl:
+      "https://drive.google.com/file/d/1ENLn7brOHSQvYTxrcawZmqfIYBHHGyzB/view",
+    href: "-",
+    tag: "Seminar",
     useIframe: true, 
   },
 ];
